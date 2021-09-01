51c324653186180e2140cda6e9c4ad257abfb961
RichardUG <rsug.rsug@gmail.com>
2021-09-01 14:52:22 -0500

    puerto

--- src/main/java/edu/escuelaing/arep/HttpServer/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer/HttpServer.java
@@ -17,7 +17,7 @@ public class HttpServer {
     public void start(String[] args) throws IOException{
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(35000);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -146,6 +146,13 @@ public class HttpServer {
             }
         }
         return output;
+    }
+
+    public int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 35000;
     }
 
 }
